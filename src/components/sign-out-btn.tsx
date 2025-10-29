8b7036931cd15dcf46e2a4464562aed61d56ebff
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-10-29 13:11:01 +0200

    useTransition for logout

--- src/components/sign-out-btn.tsx
+++ src/components/sign-out-btn.tsx
@@ -1,7 +1,20 @@
 "use client";
 import { logOut } from "@/actions/actions";
 import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 export default function SignOutBtn() {
-  return <Button onClick={async () => await logOut()}>Sign out</Button>;
+  const [isPending, startTransition] = useTransition();
+  return (
+    <Button
+      disabled={isPending}
+      onClick={async () => {
+        startTransition(async () => {
+          await logOut();
+        });
+      }}
+    >
+      Sign out
+    </Button>
+  );
 }
